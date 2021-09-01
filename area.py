b82eb9ace1803a56de5e7d2ce6dff1173e5a732d
qwur <79759207+qwur@users.noreply.github.com>
2021-09-01 16:39:24 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,3 @@
 import math
 r = input("Input radius:")
-r = int(r)
-a = math.pi*r**2
-print("area of circle with raidus "+str(r)+" is "+str(a))
-
+print("area of circle with raidus "+r+" is "+str(math.pi*int(r)**2))
